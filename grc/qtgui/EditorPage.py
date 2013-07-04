8ae01313945aa1cd3543c097caa39db49fc103ae
Sebastian Koslowski <koslowski@kit.edu>
2013-07-04 17:31:07 +0200

    Block library widget, drag&drop

---
+++ grc/qtgui/EditorPage.py
@@ -0,0 +1,178 @@
+"""
+Copyright 2008, 2009, 2011 Free Software Foundation, Inc.
+This file is part of GNU Radio
+
+GNU Radio Companion is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+GNU Radio Companion is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+"""
+
+import os
+
+from PyQt4.QtGui import *
+from PyQt4 import QtCore
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+from . DrawingArea import DrawingArea
+#from StateCache import StateCache
+
+############################################################
+## Notebook Page
+############################################################
+
+class EditorPage(QWidget):
+    """A page in the notebook."""
+
+    def __init__(self, main_window, flow_graph, file_path=''):
+        """
+        Page constructor.
+
+        Args:
+            main_window: main window
+            file_path: path to a flow graph file
+        """
+        QWidget.__init__(self)
+
+        self.main_window = main_window
+        self._flow_graph = flow_graph
+
+        self.set_proc(None)
+        self.set_file_path(file_path)
+        self.set_saved(True)
+
+        #import the file
+
+        #initial_state = flow_graph.get_parent().parse_flow_graph(file_path)
+        self.state_cache = None # StateCache(initial_state)
+        #import the data to the flow graph
+        #self.get_flow_graph().import_data(initial_state)
+
+        #initialize page gui
+        self.drawing_area = DrawingArea(self, flow_graph)
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.drawing_area)
+        layout.setMargin(0)
+
+        #self.label = None
+        #self.get_flow_graph().drawing_area = self.get_drawing_area()
+
+    def get_drawing_area(self):
+        return self.drawing_area
+
+    def get_generator(self):
+        """
+        Get the generator object for this flow graph.
+
+        Returns:
+            generator
+        """
+        return self.get_flow_graph().get_parent().get_generator()(
+            self.get_flow_graph(),
+            self.get_file_path(),
+        )
+
+    def set_markup(self, markup):
+        """
+        Set the markup in this label.
+
+        Args:
+            markup: the new markup text
+        """
+        self.label.set_markup(markup)
+
+    def get_proc(self):
+        """
+        Get the subprocess for the flow graph.
+
+        Returns:
+            the subprocess object
+        """
+        return self.process
+
+    def set_proc(self, process):
+        """
+        Set the subprocess object.
+
+        Args:
+            process: the new subprocess
+        """
+        self.process = process
+
+    def get_flow_graph(self):
+        """
+        Get the flow graph.
+
+        Returns:
+            the flow graph
+        """
+        return self._flow_graph
+
+    def get_read_only(self):
+        """
+        Get the read-only state of the file.
+        Always false for empty path.
+
+        Returns:
+            true for read-only
+        """
+        if not self.get_file_path(): return False
+        return os.path.exists(self.get_file_path()) and \
+        not os.access(self.get_file_path(), os.W_OK)
+
+    def get_file_path(self):
+        """
+        Get the file path for the flow graph.
+
+        Returns:
+            the file path or ''
+        """
+        return self.file_path
+
+    def set_file_path(self, file_path=''):
+        """
+        Set the file path, '' for no file path.
+
+        Args:
+            file_path: file path string
+        """
+        self.file_path = os.path.abspath(file_path) if file_path else ''
+
+    def get_saved(self):
+        """
+        Get the saved status for the flow graph.
+
+        Returns:
+            true if saved
+        """
+        return self.saved
+
+    def set_saved(self, saved=True):
+        """
+        Set the saved status.
+
+        Args:
+            saved: boolean status
+        """
+        self.saved = saved
+
+    def get_state_cache(self):
+        """
+        Get the state cache for the flow graph.
+
+        Returns:
+            the state cache
+        """
+        return self.state_cache
--- grc/qtgui/MainWindow.py
+++ grc/qtgui/MainWindow.py
@@ -1,64 +1,250 @@
+import os
 from PyQt4.QtGui import *
-
-from PyQt4 import QtCore
+from PyQt4.QtCore import Qt
 
 from . Block import Block
-from . FlowGraph import FlowGraph
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    _fromUtf8 = lambda s: s
+from . EditorPage import EditorPage
+from . BlockLibrary import BlockLibrary
+import Messages
 
 from ui_MainWindow import Ui_MainWindow
+
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, platform=None):
         QMainWindow.__init__(self)
 
+        self._platform = platform
         self.setupUi(self)
 
+        self.blockLibrary = BlockLibrary(self, platform, self.get_flow_graph)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.blockLibrary)
+
+        self.actionLibrary.triggered.connect(self.blockLibrary.show)
         self.actionNew.triggered.connect(self.new_page)
         self.actionClose.triggered.connect(self.close_page)
         self.editorTabs.tabCloseRequested.connect(self.close_page)
         self.actionPaste.triggered.connect(self.new_block)
 
-        self.consoleDock.close()
+        # self.btwin = BlockTreeWindow(platform, self.get_flow_graph)
+
+        self.reportDock.close()
 
         new = QTreeWidgetItem()
         new.setText(0, "Test")
-        self.blockTree.insertTopLevelItem(0, new)
+        #self.blockTree.insertTopLevelItem(0, new)
 
         new2 = QTreeWidgetItem()
         new2.setText(0, "Test2")
-        new.addChild(new2)
+        #new.addChild(new2)
 
         self.new_page()
         self.new_block()
+
         # ToDo: Load UI Prefs
+        #load preferences and show the main window
+		#Preferences.load(platform)
+		#self.resize(*Preferences.main_window_size())
+		#self.flow_graph_vpaned.set_position(Preferences.reports_window_position())
+		#self.hpaned.set_position(Preferences.blocks_window_position())
+		#self.show_all()
 
+    def new_page(self, file_path='', show=False):
+        """
+        Create a new notebook page.
+        Set the tab to be selected.
 
-    def new_page(self):
-        tab = QWidget()
-        tab.flowgraph = FlowGraph(tab)
+        Args:
+        file_path: optional file to load into the flow graph
+        show: true if the page should be shown after loading
+        """
+        #if the file is already open, show the open page and return
+        if file_path and file_path in self._get_files(): #already open
+            page = self.editorTabs.children()[self._get_files().index(file_path)]
+            self._set_page(page)
+            return
 
-        layout = QHBoxLayout(tab)
-        layout.addWidget(tab.flowgraph)
-        layout.setMargin(0)
+        try: #try to load from file
+            if file_path:
+                Messages.send_start_load(file_path)
 
-        self.editorTabs.addTab(tab, _fromUtf8("New*"))
-        self.editorTabs.setCurrentWidget(tab)
+            flow_graph = None # self._platform.get_new_flow_graph()
+            #flow_graph.grc_file_path = file_path
+
+            page = EditorPage(
+                self,
+                flow_graph=flow_graph,
+                file_path=file_path,
+            )
+
+            if file_path:
+                Messages.send_end_load()
+
+        except Exception as e: #return on failure
+            Messages.send_fail_load(e)
+            if isinstance(e, KeyError) and str(e) == "'options'":
+                # This error is unrecoverable, so crash gracefully
+                exit(-1)
+                return
+
+        else:
+            #add this page to the notebook
+            self.editorTabs.addTab(page, "New*")
+
+            #self.notebook.set_tab_label_packing(page, False, False, gtk.PACK_START)
+            #only show if blank or manual
+            if not file_path or show:
+                self.editorTabs.setCurrentWidget(self)
+
+    def close_pages(self):
+        """
+        Close all the pages in this notebook.
+
+        Returns:
+            true if all closed
+        """
+        open_files = filter(lambda f: f, self._get_files()) #filter blank files
+        open_file = self.get_page().get_file_path()
+        #close each page
+        for page in self._iter_pages():
+            self.close_page(page)
+        if self.notebook.count():
+            return False
+        #save state before closing
+        #Preferences.files_open(open_files)
+        #Preferences.file_open(open_file)
+        #Preferences.main_window_size(self.get_size())
+        #Preferences.reports_window_position(self.flow_graph_vpaned.get_position())
+        #Preferences.blocks_window_position(self.hpaned.get_position())
+        #Preferences.save()
+        return True
 
     def close_page(self, index=None):
+        """
+        Close the current page.
+        If the notebook becomes empty, and ensure is true,
+        call new page upon exit to ensure that at least one page exists.
+
+        Args:
+            index: int
+        """
         if index is None:
             index = self.editorTabs.currentIndex()
+        page_to_be_closed = self.editorTabs.widget(index)
+        #show the page if it has an executing flow graph or is unsaved
+        if page_to_be_closed.get_proc() or not page_to_be_closed.get_saved():
+            self._set_page(self.page_to_be_closed)
+
+        #unsaved? ask the user
+        if not page_to_be_closed.get_saved() and self._save_changes():
+            #Actions.FLOW_GRAPH_SAVE() #try to save
+            if not page_to_be_closed.get_saved(): #still unsaved?
+                return
+        #stop the flow graph if executing
+        #if self.page_to_be_closed.get_proc():
+        #    Actions.FLOW_GRAPH_KILL()
+        #remove the page
         self.editorTabs.removeTab(index)
 
 
     def new_block(self):
-        graphicScene = self.editorTabs.currentWidget().flowgraph.scene()
+        graphicScene = self.get_page().get_drawing_area().scene()
         graphicScene.addItem(Block())
+
+
+    ############################################################
+    # Misc
+    ############################################################
+
+    def update(self):
+        """
+        Set the title of the main window.
+        Set the titles on the page tabs.
+        Show/hide the tab bar window.
+        """
+        current_page = self.get_page()
+        self.setWindowTitle("{saved}{base}{ro}{dir}{platform}".format(
+            saved='*' if current_page.get_saved() else '',
+            base=os.path.basename(current_page.get_file_path()),
+            ro=' (read only)' if current_page.read_only() else '',
+            dir=os.path.dirname(current_page.get_file_path()),
+            platform=self._platform.get_name()
+        ))
+        #ToDO: set tab titles
+
+        #show/hide notebook tabs
+        self.editorTabs.tabBar().setVisible(self.editorTabs.count() > 1)
+
+    def get_page(self):
+        """
+        Get the selected page.
+
+        Returns:
+            the selected page
+        """
+        return self.editorTabs.currentWidget()
+
+    def get_flow_graph(self):
+        """
+        Get the selected flow graph.
+
+        Returns:
+            the selected flow graph
+        """
+        return self.get_page().get_flow_graph()
+
+    def get_focus_flag(self):
+        """
+        Get the focus flag from the current page.
+
+        Returns:
+            the focus flag
+        """
+        return self.get_page().get_drawing_area().get_focus_flag()
+
+
+    ############################################################
+    # Helpers
+    ############################################################
+
+    def _set_page(self, page):
+        """
+        Set the current page.
+
+        Args:
+            page: the page widget
+        """
+        self.current_page = page
+        self.editorTabs.setCurrentWidget(page)
+
+    def _save_changes(self):
+        """
+        Save changes to flow graph?
+
+        Returns:
+            true if yes
+        """
+        return QMessageBox.question(self, 'Unsaved Changes!',
+                             'Would you like to save changes before closing?',
+                             QMessageBox.Yes, QMessageBox.No) == QMessageBox.Yes
+
+    def _get_files(self):
+        """
+        Get the file names for all the pages, in order.
+
+        Returns:
+            list of file paths
+        """
+        return map(lambda page: page.get_file_path(), self._get_pages())
+
+    def _iter_pages(self):
+        """
+        Iterate over all pages in the notebook.
+        """
+        for i in range(self.editorTabs.count()):
+            yield self.editorTabs.widget(i)
+
 
 
 
