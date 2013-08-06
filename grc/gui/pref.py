db98503d0d0be9513ca0d02dac0cbcec8454a7d6
Aneela Yasmeen <u04181@yahoo.com>
2013-08-05 22:58:50 -0700

    removed unnecessary files

--- grc/gui/pref.py
+++
@@ -1,6 +0,0 @@
-from gnuradio import gr
-class myclass:
-	def __init__(self):
-		print "-----------------------------------------------"
-		print gr.prefs().get_string('my', 'cc_editor', 'Gedit')
-		print "-----------------------------------------------"
